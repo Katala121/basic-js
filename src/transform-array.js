1251106695bc52c2e3873985e49bc90b77193d74
Katala121 <sanyafootb@list.ru>
2021-03-28 23:55:11 +0300

    feat: implement fanctions

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,20 @@
-const CustomError = require("../extensions/custom-error");
-
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  let returnArr = []
+  // console.log(arr)
+  arr.map((item, i) => {
+    if (item == '--discard-next') {
+      if (arr[i+1] && typeof arr[i+1] == 'number') arr.splice(i + 1, 1)
+    }
+    else if (item == '--discard-prev') {
+      if (arr[i-1] && typeof arr[i-1] == 'number') returnArr.pop()
+    }
+    else if (item == '--double-next') {
+      if (arr[i+1] && typeof arr[i+1] == 'number') returnArr.push(arr[i + 1])
+    }
+    else if (item == '--double-prev') {
+      if (arr[i-1] && typeof arr[i-1] == 'number') returnArr.push(arr[i - 1])
+    }
+    else returnArr.push(item)
+  })
+  return returnArr
 };
--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,10 @@
-const CustomError = require("../extensions/custom-error");
-
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+  if(!arguments.length) return 'Unable to determine the time of year!'
+  date.setDate(10)
+  if (!(date instanceof Date)) throw Error
+  if (date.getMonth() < 2) return 'winter'
+  else if (date.getMonth() < 5) return 'spring'
+  else if (date.getMonth() < 8) return 'summer'
+  else if (date.getMonth() < 11) return 'autumn'
+  else if (date.getMonth() > 10) return 'winter'
 };
