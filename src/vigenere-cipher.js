42c9201651da911f35a19a65378e2701ad4d24ea
Katala121 <sanyafootb@list.ru>
2021-03-30 22:33:40 +0300

    feat: implement fanctions

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -5,16 +5,29 @@ class VigenereCipheringMachine {
   }
   encrypt(message, key) {
     if (!(message && key)) throw Error()
+    let a = 0
     return message.split('').map((item, i) => {
       if (!item.match(/[a-zA-Z]/)) return item
-      // let a = i
-      if (i > key.length) while (a >= key.length) a - key.length
-      const x = this.letters.indexOf(item.toUpperCase()) + this.letters.indexOf(key[a])
-      return x > 26 ? this.letters[x - 26] : this.letters[x] 
+      if (a > key.length - 1){
+        a -= key.length
+      }
+      const x = this.letters.indexOf(item.toUpperCase()) + this.letters.indexOf(key[a].toUpperCase())
+      a++
+      return x >= 26 ? this.letters[x - 26] : this.letters[x] 
     }).join('')
   }    
   decrypt(message, key) {
     if (!(message && key)) throw Error()
+    let a = 0
+    return message.split('').map((item, i) => {
+      if (!item.match(/[a-zA-Z]/)) return item
+      if (a > key.length - 1){
+        a -= key.length
+      }
+      const x = this.letters.indexOf(item.toUpperCase()) - this.letters.indexOf(key[a].toUpperCase())
+      a++
+      return x < 0 ? this.letters[x + 26] : this.letters[x]
+    }).join('')
   }
 }
 
